b2d0b80b5262af804a8951940fc8876e6ef78d20
Shay Raize Meshulam <shayros@gmail.com>
2023-02-22 11:39:30 +0200

    Update create_DL_links.sh

--- create_DL_links.sh
+++ create_DL_links.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+echo " The links below are valid for 7 days only..."
+if [ -z "$1" ];
+then
+        echo "please provide s3 url to generate the link";
+        echo "for example: ./aws_create_presign.sh s3://<path>/filename.tar.gz";
+        echo "$0 should be fixed";
 
-FILES=(AA BB)
+else
+        LINK=$(aws s3 presign --region us-east-1 --expires-in 604800 $1);
+        #echo "Download link: $LINK";
+        F1=($(echo $1 | awk -F / '{print $NF}'));
+        F2=$(awk '{print $5}' <<< $F1);
 
-for f in {FILES[@]};
-do
-	echo "Generating link to file $f";
-done
+        echo "Run the command below to download:";
+        echo -e "wget -O /tmp/$F1 ${LINK}";
 
+fi
